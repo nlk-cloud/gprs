4ece3a118db6111e5e0238fde393a420782fb0e2
nlk-cloud <100998430+nlk-cloud@users.noreply.github.com>
2022-06-24 20:03:31 +0530

    Add files via upload

---
+++ gprs.ino
@@ -0,0 +1,142 @@
+/*THIS TUTORIAL USED GSM SIM900A MINI V3.9.2
+ 
+  Connect 5VT to D9 and 5VR to D10
+  Feed GSM SIM900A with Arduino's 5V
+
+  Code by IDAYU SABRI - MYBOTIC
+*/
+/*
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(9, 10);
+char msg;
+char call;
+int j;
+void setup()
+{
+  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+  Serial.println("GSM SIM900A BEGIN");
+  Serial.println("Enter character for control option:");
+  Serial.println("h : to disconnect a call");
+  Serial.println("i : to receive a call");
+  Serial.println("s : to send message");
+  Serial.println("c : to make a call");  
+  Serial.println("e : to redial");
+  Serial.println("m : to receive message");  
+  Serial.println();
+  delay(100);
+}
+
+void loop()
+{  
+  if (Serial.available()>0)
+   switch(Serial.read())
+  {
+    case 's':
+      SendMessage();
+      break;
+    case 'm':
+      ReceiveMessage();
+      break;    
+    case 'c':
+      MakeCall();
+      break;
+    case 'h':
+      HangupCall();
+      break;
+    case 'e':
+      RedialCall();
+      break;
+    case 'i':
+      ReceiveCall();
+      break;
+  }
+ if (mySerial.available()>0)
+ Serial.write(mySerial.read());
+}
+
+void SendMessage()
+{
+  mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  mySerial.println("AT+CMGS=\"+919611839500\"\r"); // Replace x with mobile number
+  delay(1000);
+  mySerial.println("sim900a sms");// The SMS text you want to send
+  delay(100);
+   mySerial.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+}
+
+void ReceiveMessage()
+{
+  mySerial.println("AT+CNMI=2,2,0,0,0"); // AT Command to recieve a live SMS
+  delay(1000);
+  mySerial.println("AT+CMGR=1");  
+  if (mySerial.available()>0)
+  {
+   // if (mySerial.read()=='+')
+  //{    
+    //for (j=0; j<10; j++ )
+    //  {    
+        msg=mySerial.read();
+        Serial.print(msg);
+  //    }
+  //}
+
+  //else 
+//Serial.print("couldnt find");  
+  
+  }
+}  
+
+void MakeCall()
+{
+  mySerial.println("ATD+919611839500;"); // ATDxxxxxxxxxx; -- watch out here for semicolon at the end!!
+  Serial.println("Calling  "); // print response over serial port
+  delay(1000);
+}
+
+
+void HangupCall()
+{
+  mySerial.println("ATH");
+  Serial.println("Hangup Call");
+  delay(1000);
+}
+
+void ReceiveCall()
+{
+  mySerial.println("ATA");
+  delay(1000);
+  {
+    call=mySerial.read();
+    Serial.print(call);
+  }
+}
+
+void RedialCall()
+{
+  mySerial.println("ATDL");
+  Serial.println("Redialing");
+  delay(1000);
+}
+*/
+
+// USe to the below code to send AT commands to the GSM module
+
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(9, 10); // RX, TX
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+void loop() {
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
+
